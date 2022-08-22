76f0e6ddd3649c738226bc4f4ee9f142fc222ee5
nikita <nikitakovalec@gmail.com>
2022-08-22 10:59:43 +0300

    fix code

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
+import {ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
+import {CacheProvider} from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 import {useState} from "react";
@@ -13,40 +13,40 @@ import {UserContext} from "../context"
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props, {userObj}) {
-	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-	const [user, setUser] = useState(userObj || null)
+  const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+  const [user, setUser] = useState(userObj || null)
 
-	return (
-		<UserContext.Provider value={{user, setUser}}>
-		<CacheProvider value={emotionCache}>
-			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
-			</Head>
-			<ThemeProvider theme={theme}>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</CacheProvider>
-		</UserContext.Provider>
-	);
+  return (
+    <UserContext.Provider value={{user, setUser}}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width"/>
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline/>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </UserContext.Provider>
+  );
 }
 
 export async function getServerSideProps(context) {
-	const res = await fetch('http://localhost:3000/')
-	const userObj = await res.json()
-	if (!userObj) {
-		return {
-			props: null
-		}
-	}
-	return {
-		props: {userObj}
-	}
+  const res = await fetch('http://localhost:3000/')
+  const userObj = await res.json()
+  if (!userObj) {
+    return {
+      props: null
+    }
+  }
+  return {
+    props: {userObj}
+  }
 }
 
 MyApp.propTypes = {
-	Component: PropTypes.elementType.isRequired,
-	emotionCache: PropTypes.object,
-	pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
 };
